31e6120d915f23fe72305474c1ba4bd4ec69ede6
Utsav Gole <87436654+UtsavGole@users.noreply.github.com>
2023-08-30 21:29:51 +0545

    Add files via upload

---
+++ Day1/day1.py
@@ -0,0 +1,6 @@
+print("Welcome to the brand name generator!!")
+city_name=input("Enter your city name: ")
+pet_name=input("Enter your pet name: ")
+row=f"your band name is {city_name} {pet_name}."
+print(row)
+print("Thanyou!'/n' programe is completed")
